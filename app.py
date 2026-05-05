b74bb2ac936d95bc84e6ba72444c46c741fe20d0
simransoni54575-hub <simransoni54575@gmail.com>
2026-05-05 13:45:07 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,25 +2,22 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-# HOME PAGE
 @app.route('/')
 def home():
     return render_template('index.html')
 
-
-# PREDICT ROUTE
 @app.route('/predict', methods=['POST'])
 def predict():
     url = request.form['url']
 
-    # simple working logic (no ML crash)
     if "https" in url:
         result = "Safe Website ✅"
+        color = "green"
     else:
         result = "Phishing Website ⚠️"
+        color = "red"
 
-    return f"<h2>{result}</h2><br><a href='/'>Go Back</a>"
-
+    return render_template('index.html', result=result, color=color)
 
 if __name__ == "__main__":
     app.run()
