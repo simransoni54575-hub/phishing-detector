a32de1faf5b87ae2f55a6ead97a87b22aaabe2ce
simransoni54575-hub <simransoni54575@gmail.com>
2026-05-05 14:15:29 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html', result=None)
+
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
-        url = request.form['url']
+        url = request.form.get('url')
 
-        # TEMP LOGIC (demo)
+        if not url:
+            return render_template('index.html', result="No URL entered ⚠️")
+
+        # TEMP LOGIC
         if "https" in url:
             prediction = 0
         else:
@@ -17,4 +28,8 @@ def predict():
         return render_template('index.html', result=result)
 
     except Exception as e:
+        print("ERROR:", e)   # logs me dikhega
         return render_template('index.html', result="Error occurred ⚠️")
+
+if __name__ == "__main__":
+    app.run()
