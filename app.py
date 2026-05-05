dec9b7dfec3494b0de92f1c504647488c0f1e4a5
simransoni54575-hub <simransoni54575@gmail.com>
2026-05-05 13:49:18 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,10 @@
-from flask import Flask, request, render_template
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return render_template('index.html')
-
 @app.route('/predict', methods=['POST'])
 def predict():
     url = request.form['url']
 
     if "https" in url:
         result = "Safe Website ✅"
-        color = "green"
     else:
         result = "Phishing Website ⚠️"
-        color = "red"
 
-    return render_template('index.html', result=result, color=color)
-
-if __name__ == "__main__":
-    app.run()
+    return render_template("index.html", result=result)
